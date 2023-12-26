b9d4f374e7f0deaab5bbd74c7149552eebf240a8
mehdi <mehdizolfghari25@gmail.com>
2023-12-26 16:23:12 +0330

    all files added

---
+++ js/jscode.js
@@ -0,0 +1,8 @@
+//for responsive nav
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "220px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
